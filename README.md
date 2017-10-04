7494203a17ff76c0fecd2e394d7007888856a796
Pavel <pavepy@yandex.ru>
2017-10-04 13:49:11 +0300

    test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Репозиторий для обучения
 Здесь скоро будут файлы и ссылки, которые помогут вам в обучении веб-разработке
+
+
+# Программы для работы
+* node.js-сервер
+* git
+
+# консольные команды для запуска
+
+cd c:/
+mkdir myFolder
+cd myFolder
+git clone "https://github.com/Carduelis/teaching-example.git";
+npm install
+npm run web-dev
--- app/index.js
+++ app/index.js
@@ -3,44 +3,39 @@ import url from "./url";
 import "./styles/style.less";
 import Trianglify from "trianglify";
 const rootElement = document.getElementById("root");
-var pattern = Trianglify({
-	width: window.innerWidth,
-	height: window.innerHeight
-});
-rootElement.appendChild(pattern.canvas());
-// @isTestable(true)
-// class MyClass {}
-//
-// function isTestable(value) {
-// 	return function decorator(target) {
-// 		target.isTestable = value;
-// 	};
-// }
 
-setTimeout(() => {
-	ajax({
-		url,
-		// jsonp: true,
-		debug: true,
-		withCredentials: true,
-		success: (...rest) => {
-			console.log(...rest);
-		},
-		error: (...rest) => {
-			console.error(...rest);
-		}
-	}).then(response => console.log(response), error => console.error(error));
-	console.log("end");
-}, 1000);
+var ourDiv = document.createElement("DIV");
+function setStyle(target) {
+	target.style.height = window.innerHeight/2 + "px";
+	const hue = window.innerWidth;
+	target.style.backgroundColor = 'hsl('+hue+', 50%, 50%)';
+}
+setStyle(ourDiv);
+// var pattern = Trianglify({
+// 	width: window.innerWidth,
+// 	height: window.innerHeight
+// });
+rootElement.appendChild(ourDiv);
 
-class Rabbit {
-	constructor(name) {
-		this.name = name;
-	}
-	sayHi() {
-		return "My name is " + this.name;
-	}
+const myResize = function(event) {
+	setStyle(ourDiv);
 }
 
-const vasya = new Rabbit("Вася");
-vasya.sayHi();
+window.onresize = myResize;
+
+// setTimeout(() => {
+// 	ajax({
+// 		url,
+// 		// jsonp: true,
+// 		debug: true,
+// 		withCredentials: true,
+// 		success: (...rest) => {
+// 			console.log(...rest);
+// 		},
+// 		error: (...rest) => {
+// 			console.error(...rest);
+// 		}
+// 	}).then(response => console.log(response), error => console.error(error));
+// 	console.log("end");
+// }, 1000);
+//
